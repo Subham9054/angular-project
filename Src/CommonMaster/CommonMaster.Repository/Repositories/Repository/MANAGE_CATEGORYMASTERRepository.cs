949ed5b7a9ec719380f9b0d8b7c827f704d6bdbd
subham.d <subham.d@csm.tech>
2024-11-05 11:30:30 +0530

    Changes In Category

--- Src/CommonMaster/CommonMaster.Repository/Repositories/Repository/MANAGE_CATEGORYMASTERRepository.cs
+++ Src/CommonMaster/CommonMaster.Repository/Repositories/Repository/MANAGE_CATEGORYMASTERRepository.cs
@@ -7,6 +7,8 @@ using CommonMaster.Repository;
 using PHED_CGRC.MANAGE_CATEGORYMASTER;
 using Dapper;
 using System.Data;
+using CommonMaster.Model.Entities.CommonMaster;
+using MySqlConnector;
 namespace CommonMaster.Repository.Repositories.Interfaces.MANAGE_CATEGORYMASTER
 {
     public class MANAGE_CATEGORYMASTERRepository : db_PHED_CGRCRepositoryBase, IMANAGE_CATEGORYMASTERRepository
@@ -15,121 +17,81 @@ namespace CommonMaster.Repository.Repositories.Interfaces.MANAGE_CATEGORYMASTER
         {
 
         }
-        public async Task<int> INSERT_MANAGE_CATEGORYMASTER(MANAGE_CATEGORYMASTER_Model TBL)
+        public async Task<bool> ComplaintCatagory(ComplaintCategory complaintCategory)
         {
-            var p = new DynamicParameters();
-            p.Add("@Action ", "INSERT");
-            p.Add("@CategoryId", TBL.CategoryId);
-            p.Add("@CategoryE", TBL.CategoryE);
-            p.Add("@CategoryH", TBL.CategoryH);
-            p.Add("@DeletedFlag", TBL.DeletedFlag);
-            p.Add("@UserId", TBL.UserId);
-            p.Add("@Msg", dbType: DbType.String, direction: ParameterDirection.Output, size: 5215585);
-            var results = await Connection.ExecuteAsync("MANAGE_CATEGORYMASTER", p, commandType: CommandType.StoredProcedure);
-            int Param = Convert.ToInt32(p.Get<string>("@Msg"));
-            return Param;
-
-        }
-        public async Task<int> UPDAE_MANAGE_CATEGORYMASTER(MANAGE_CATEGORYMASTER_Model TBL)
-        {
-            var p = new DynamicParameters();
-            p.Add("@Action ", "UPDAE");
-            p.Add("@CategoryId", TBL.CategoryId);
-            p.Add("@CategoryE", TBL.CategoryE);
-            p.Add("@CategoryH", TBL.CategoryH);
-            p.Add("@DeletedFlag", TBL.DeletedFlag);
-            p.Add("@UserId", TBL.UserId);
-            p.Add("@Msg", dbType: DbType.String, direction: ParameterDirection.Output, size: 5215585);
-            var results = await Connection.ExecuteAsync("MANAGE_CATEGORYMASTER", p, commandType: CommandType.StoredProcedure);
-            int Param = Convert.ToInt32(p.Get<string>("@Msg"));
-            return Param;
-
-        }
-        public async Task<int> DELETE_MANAGE_CATEGORYMASTER(MANAGE_CATEGORYMASTER_Model TBL)
-        {
-            var p = new DynamicParameters();
-            p.Add("@Action ", "DELETE");
-            p.Add("@CategoryId", TBL.CategoryId);
-            p.Add("@CategoryE", TBL.CategoryE);
-            p.Add("@CategoryH", TBL.CategoryH);
-            p.Add("@DeletedFlag", TBL.DeletedFlag);
-            p.Add("@UserId", TBL.UserId);
-            p.Add("@Msg", dbType: DbType.String, direction: ParameterDirection.Output, size: 5215585);
-            var results = await Connection.ExecuteAsync("MANAGE_CATEGORYMASTER", p, commandType: CommandType.StoredProcedure);
-            int Param = Convert.ToInt32(p.Get<string>("@Msg"));
-            return Param;
-
-        }
-        public class VIEW_MANAGE_CATEGORYMASTERMapper
-        {
-            public Dapper.SqlMapper.ITypeMap VIEW_MANAGE_CATEGORYMASTERmapper()
             {
-                var map = new CustomPropertyTypeMap(typeof(VIEWMANAGE_CATEGORYMASTER), (type, columnName) =>
+                try
                 {
-                    if (columnName == "CategoryId") return type.GetProperty("CategoryId");
-                    if (columnName == "CategoryE") return type.GetProperty("CategoryE");
-                    if (columnName == "CategoryH") return type.GetProperty("CategoryH");
-                    if (columnName == "CreatedBy") return type.GetProperty("CreatedBy");
-                    if (columnName == "CreatedOn") return type.GetProperty("CreatedOn");
-                    if (columnName == "UpdatedBy") return type.GetProperty("UpdatedBy");
-                    if (columnName == "UpdatedOn") return type.GetProperty("UpdatedOn");
-                    if (columnName == "DeletedFlag") return type.GetProperty("DeletedFlag");
-                    return null;
-                });
-                return map;
+                    DynamicParameters dynamicParameters = new DynamicParameters();
+                    dynamicParameters.Add("@vchcategory", complaintCategory.VCH_CATEGORY);
+                    dynamicParameters.Add("@nvchcategory", complaintCategory.NVCH_CATEGORY);
+                    dynamicParameters.Add("@createdby", 1);
+                    dynamicParameters.Add("@createdon", DateTime.Now);
+                    dynamicParameters.Add("@action", "CI");
+                    var result = await Connection.QueryAsync<int>("USP_ComplaintCategory", dynamicParameters, commandType: CommandType.StoredProcedure);
+                    return result.Contains(1);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
             }
         }
-        public async Task<List<VIEWMANAGE_CATEGORYMASTER>> VIEW_MANAGE_CATEGORYMASTER(MANAGE_CATEGORYMASTER_Model TBL)
+        public async Task<List<ComplaintCategory>> getCatagory()
         {
-            var p = new DynamicParameters();
-            p.Add("@Action ", "VIEW");
-            p.Add("@CategoryId", TBL.CategoryId);
-            p.Add("@CategoryE", TBL.CategoryE);
-            p.Add("@CategoryH", TBL.CategoryH);
-            p.Add("@UserId", TBL.UserId);
-            p.Add("@DeletedFlag", TBL.DeletedFlag);
-            
-            p.Add("@Msg", dbType: DbType.String, direction: ParameterDirection.Output, size: 5215585);
-            VIEW_MANAGE_CATEGORYMASTERMapper _Mapper = new VIEW_MANAGE_CATEGORYMASTERMapper();
-            Dapper.SqlMapper.SetTypeMap(typeof(VIEWMANAGE_CATEGORYMASTER), _Mapper.VIEW_MANAGE_CATEGORYMASTERmapper());
-            var results = await Connection.QueryAsync<VIEWMANAGE_CATEGORYMASTER>("MANAGE_CATEGORYMASTER", p, commandType: CommandType.StoredProcedure);
-            return results.ToList();
-
-        }
-        public class EDIT_MANAGE_CATEGORYMASTERMapper
-        {
-            public Dapper.SqlMapper.ITypeMap EDIT_MANAGE_CATEGORYMASTERmapper()
             {
-                var map = new CustomPropertyTypeMap(typeof(EDITMANAGE_CATEGORYMASTER), (type, columnName) =>
+                try
                 {
-                    if (columnName == "CategoryId") return type.GetProperty("CategoryId");
-                    if (columnName == "CategoryE") return type.GetProperty("CategoryE");
-                    if (columnName == "CategoryH") return type.GetProperty("CategoryH");
-                    if (columnName == "CreatedBy") return type.GetProperty("CreatedBy");
-                    if (columnName == "CreatedOn") return type.GetProperty("CreatedOn");
-                    if (columnName == "UpdatedBy") return type.GetProperty("UpdatedBy");
-                    if (columnName == "UpdatedOn") return type.GetProperty("UpdatedOn");
-                    if (columnName == "DeletedFlag") return type.GetProperty("DeletedFlag");
-                    return null;
-                });
-                return map;
+                    DynamicParameters dynamicParameters = new DynamicParameters();
+                    dynamicParameters.Add("@action", "CG");
+                    var categories = await Connection.QueryAsync<ComplaintCategory>("USP_getallComplaintCategory", dynamicParameters, commandType: CommandType.StoredProcedure);
+                    return categories.ToList();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
             }
         }
-        public async Task<List<EDITMANAGE_CATEGORYMASTER>> EDIT_MANAGE_CATEGORYMASTER(MANAGE_CATEGORYMASTER_Model TBL)
+        public async Task<int> getdeleteCatagorybyid(int id)
         {
-            var p = new DynamicParameters();
-            p.Add("@Action ", "EDIT");
-            p.Add("@CategoryId", TBL.CategoryId);
-            p.Add("@CategoryE", TBL.CategoryE);
-            p.Add("@CategoryH", TBL.CategoryH);
-            p.Add("@DeletedFlag", TBL.DeletedFlag);
-            p.Add("@UserId", TBL.UserId);
-            p.Add("@Msg", dbType: DbType.String, direction: ParameterDirection.Output, size: 5215585);
-            EDIT_MANAGE_CATEGORYMASTERMapper _Mapper = new EDIT_MANAGE_CATEGORYMASTERMapper();
-            Dapper.SqlMapper.SetTypeMap(typeof(EDITMANAGE_CATEGORYMASTER), _Mapper.EDIT_MANAGE_CATEGORYMASTERmapper());
-            var results = await Connection.QueryAsync<EDITMANAGE_CATEGORYMASTER>("MANAGE_CATEGORYMASTER", p, commandType: CommandType.StoredProcedure);
-            return results.ToList();
+            {
+                try
+                {
+                    DynamicParameters dynamicParameters = new DynamicParameters();
+                    dynamicParameters.Add("@id", id);
 
+                    // Execute the stored procedure and retrieve the affected row count
+                    var result = await Connection.ExecuteScalarAsync<int>("USP_deleteComplaintCategorybyid", dynamicParameters, commandType: CommandType.StoredProcedure);
+
+                    return result;  // This will return the number of rows affected (e.g., 1 for success)
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+        }
+        public async Task<bool> UpdateComplaintCatagory(int id, ComplaintCategory complaintCategory)
+        {
+            {
+                try
+                {
+                    DynamicParameters dynamicParameters = new DynamicParameters();
+                    dynamicParameters.Add("@id", id);
+                    dynamicParameters.Add("@vchcategory", complaintCategory.VCH_CATEGORY);
+                    dynamicParameters.Add("@nvchcategory", complaintCategory.NVCH_CATEGORY);
+                    dynamicParameters.Add("@updatedby", 1);
+                    dynamicParameters.Add("@updatedon", DateTime.Now);
+                    dynamicParameters.Add("@action", "CU");
+                    var result = await Connection.QueryAsync<int>("USP_UpdateComplaintCategory", dynamicParameters, commandType: CommandType.StoredProcedure);
+                    return result.Contains(1);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
