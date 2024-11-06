00683eb5debdebe55582879103ad3660254a91ff
bijayalaxmi.patra <bijayalaxmi.patra@csm.tech>
2024-11-06 09:22:57 +0530

    Design Change

---
+++ phed_ui/src/app/application/complaint-sub-category/complaint-sub-category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complaint-sub-category',
+  templateUrl: './complaint-sub-category.component.html',
+  styleUrls: ['./complaint-sub-category.component.scss']
+})
+export class ComplaintSubCategoryComponent {
+
+}
