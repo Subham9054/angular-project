d8b3f755b5dbe35d687fcf22dee000d46a9e96ee
subham.d <subham.d@csm.tech>
2024-11-12 21:01:29 +0530

    changes

---
+++ Src/GMS/GMS.Model/Entities/GMS/Complaint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMS.Model.Entities.GMS
+{
+    public class Complaint
+    {
+        public int INT_CATEGORY_ID { get; set; }
+        public int INT_COMPLIANT_LOG_TYPE { get; set; }
+        public int INT_SUB_CATEGORY_ID { get; set; }
+        public string VCH_CONTACT_NO { get; set; }
+        public string NVCH_COMPLIANTANT_NAME { get; set; }
+        public int INT_DIST_ID { get; set; }
+        public int INT_BLOCK { get; set; }
+        public int INT_PANCHAYAT { get; set; }
+        public int INT_VILLAGE { get; set; }
+        public string NVCH_ADDRESS { get; set; }
+        public string NVCH_COMPLIANT_DETAILS { get; set; }
+        public string VCH_COMPLAINT_FILE { get; set; }
+        public string VCH_TOKENNO { get; set; }
+        public DateTime DTM_CREATED_ON { get; set; } // Assuming it will be provided from frontend or generated in the repository
+        public string VCH_EMAIL { get; set; }
+        public string NVCH_LANDMARK { get;set; }
+    }
+}
