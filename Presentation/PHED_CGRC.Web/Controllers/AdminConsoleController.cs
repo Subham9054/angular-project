c54dfc7ad82eee676d9ccad43c6f9aea8ef802c7
subratkdash <subrat.dash@csm.tech>
2024-11-14 12:00:55 +0530

    ok

---
+++ Presentation/PHED_CGRC.Web/Controllers/AdminConsoleController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace PHED_CGRC.Web.Controllers
+{
+    public class AdminConsoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddGlobalLink()
+        {
+            var s = HttpContext.Session.GetString("UserName");
+            if (!string.IsNullOrEmpty(HttpContext.Session.GetString("UserId")))
+            {
+              
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+
+        }
+    }
+}
