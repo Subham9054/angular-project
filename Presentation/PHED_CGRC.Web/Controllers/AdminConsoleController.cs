341d25387467e98677cae110995d4f281fceeeab
subratkdash <subrat.dash@csm.tech>
2024-11-18 15:54:00 +0530

    ok

--- Presentation/PHED_CGRC.Web/Controllers/AdminConsoleController.cs
+++ Presentation/PHED_CGRC.Web/Controllers/AdminConsoleController.cs
@@ -12,16 +12,7 @@ namespace PHED_CGRC.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> AddGlobalLink()
         {
-            var s = HttpContext.Session.GetString("UserName");
-            if (!string.IsNullOrEmpty(HttpContext.Session.GetString("UserId")))
-            {
-              
-                return View();
-            }
-            else
-            {
-                return RedirectToAction("Login", "Home");
-            }
+            return View();
 
 
         }
