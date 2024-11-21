49a5e6811d80797d35f40ad10d9310bf8fcded3b
subham.d <subham.d@csm.tech>
2024-11-21 13:02:29 +0530

    Website Part Added

---
+++ Website.Repository/Container/CustomContainer.cs
@@ -0,0 +1,23 @@
+﻿using Dropdown.Repository.Factory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Website.Repository.Repositories.Interfaces;
+using Website.Repository.Repositories.Repository;
+
+namespace Website.Repository.Container
+{
+    public static class CustomContainer
+    {
+        public static void AddCustomContainer(this IServiceCollection services, IConfiguration configuration)
+        {
+            Idb_PHED_CGRCConnectionFactory db_PHED_CGRCconnectionFactory = new db_PHED_CGRCConnectionFactory(configuration.GetConnectionString("DBconnectiondb_PHED_CGRC"));
+            services.AddSingleton<Idb_PHED_CGRCConnectionFactory>(db_PHED_CGRCconnectionFactory);
+            services.AddScoped<IWebsiteRepository, IWebsiteRepository>();
+        }
+    }
+}
