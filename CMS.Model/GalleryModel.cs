7bab994a0543e11994b34817037f56fdb31896cf
Debasis Das <debasis.das@csm.tech>
2024-12-16 17:31:21 +0530

    ok

---
+++ CMS.Model/GalleryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Model
+{
+    public class GalleryModel
+    {
+        public int? GalleryId { get; set; }
+        public string? GalleryCategory { get; set; }
+        public string? GalleryType { get; set; }
+        public string? Thumbnail { get; set; }
+        public string? Image { get; set; }
+        public string? Video { get; set; }
+        public string? VideoUrl { get; set; }
+        public string? CaptionEnglish { get; set; }
+        public string? CaptionHindi { get; set; }
+        public int SerialNo { get; set; }
+        public bool IsPublish { get; set; }
+        public int? CreatedBy { get; set; }
+    }
+}
